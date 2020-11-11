8b84c2f9066e48b6ec2c2a1bb94e986dc6fb95cb
Prim69 <almusawihashim@gmail.com>
2020-11-11 17:01:40 -0500

    Add files via upload

---
+++ src/xPrim69x/MultiVersion/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace xPrim69x\MultiVersion;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\LoginPacket;
+use pocketmine\network\mcpe\protocol\ProtocolInfo;
+
+class Main extends PluginBase implements Listener{
+
+	public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+    }
+
+    public function onLogin(DataPacketReceiveEvent $event) {
+        $pk = $event->getPacket();
+        if($pk instanceof LoginPacket){
+        	if($pk->protocol !== ProtocolInfo::CURRENT_PROTOCOL) {
+            $pk->protocol = ProtocolInfo::CURRENT_PROTOCOL;
+       		}
+    	}
+	}
+
+}
